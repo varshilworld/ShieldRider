9be90509abbe12c45dae80a3fb9f5bce983d27ff
varshilworld <varshilgupta@gmail.com>
2026-03-17 21:50:48 +0530

    updated ui

--- backend/app.py
+++ backend/app.py
@@ -1,26 +1,57 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
-import random
+import requests
 
 app = Flask(__name__)
 CORS(app)
 
+API_KEY = "3a8847835b137291511aac7dbe1bc628"        
+
+
 @app.route("/simulate-event")
+
 def simulate_event():
 
-    events = [
-        {"event": "Heavy Rain", "value": "65mm", "payout": 500},
-        {"event": "Extreme Heat", "value": "44°C", "payout": 300},
-        {"event": "Severe Pollution", "value": "AQI 420", "payout": 250}
-    ]
+    city = "Chennai"
 
-    event = random.choice(events)
+    url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric"
+
+    response = requests.get(url)
+
+    data = response.json()
+
+    temperature = data["main"]["temp"]
+    weather = data["weather"][0]["main"]
+
+    event = "Normal Conditions"
+    payout = 0
+    value = ""
+
+    if temperature > 40:
+
+        event = "Extreme Heat"
+        value = f"{temperature}°C"
+        payout = 300
+
+    elif weather == "Rain":
+
+        event = "Heavy Rain"
+        value = "Rain detected"
+        payout = 500
+
+    else:
+
+        event = "No Disruption"
+        value = f"{temperature}°C"
+        payout = 0
 
     return jsonify({
-        "status": "Trigger Activated",
-        "event": event["event"],
-        "value": event["value"],
-        "payout": event["payout"]
+
+        "status": "Checked Real Weather Data",
+        "event": event,
+        "value": value,
+        "payout": payout
+
     })
 
 
